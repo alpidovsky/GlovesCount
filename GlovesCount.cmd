33e1c09ee94755f88fd8b6c95a1b2e53e6f05e32
Alexander Alpidovskiy <alexander.alpidovsky@gmail.com>
2019-05-23 01:26:30 +0300

    Update GlovesCount.cmd

--- GlovesCount.cmd
+++ GlovesCount.cmd
@@ -3,3 +3,4 @@ c:
 cd C:\GlovesCount
 "C:\Program Files (x86)\Java\jre1.8.0_211\bin\java.exe" -classpath . GlovesCount
 pause
+rem В четвертой строке этого файла вот этот путь "C:\Program Files (x86)\Java\jre1.8.0_211\bin\java.exe" до файла java.exe замените на свой, где у вас лежит java.exe
